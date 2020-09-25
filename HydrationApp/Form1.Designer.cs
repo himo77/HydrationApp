d0604ba977797094519a30f50ca2cfa5936c58f1
himo77 <56316743+himo77@users.noreply.github.com>
2020-09-25 16:43:23 -0500

    Add project files.

---
+++ HydrationApp/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HydrationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudWaterQuantity = new System.Windows.Forms.NumericUpDown();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWaterQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 79);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(608, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How many glasses of water did you drink today?";
+            // 
+            // nudWaterQuantity
+            // 
+            this.nudWaterQuantity.Location = new System.Drawing.Point(420, 177);
+            this.nudWaterQuantity.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.nudWaterQuantity.Name = "nudWaterQuantity";
+            this.nudWaterQuantity.Size = new System.Drawing.Size(240, 31);
+            this.nudWaterQuantity.TabIndex = 1;
+            this.nudWaterQuantity.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(444, 312);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(246, 30);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Drink some water !";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1118, 437);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.nudWaterQuantity);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Hydration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudWaterQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudWaterQuantity;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ HydrationApp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HydrationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Setting initial color for both background and foreground
+            this.BackColor = Color.DarkGray;
+            this.ForeColor = Color.Black;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //Fetching the value from the NumericUpDown control
+            decimal waterQuantity = this.nudWaterQuantity.Value;
+
+            //Based on the value, choosing the right message and
+            //the background and foreground colors.
+            if (waterQuantity >= 0 && waterQuantity <= 1)
+            {
+                this.lblStatus.Text = "Drink some water!";
+                this.BackColor = Color.DarkGray;
+                this.ForeColor = Color.Black;
+            }
+            else if (waterQuantity >= 2 && waterQuantity <= 3)
+            {
+                this.lblStatus.Text = "Good job!";
+                this.BackColor = Color.LightBlue;
+                this.ForeColor = Color.Black;
+            }
+            else if (waterQuantity >= 4 && waterQuantity <= 6)
+            {
+                this.lblStatus.Text = "Awesome!";
+                this.BackColor = Color.MediumAquamarine;
+                this.ForeColor = Color.Black;
+            }
+            else if (waterQuantity >= 6 && waterQuantity <= 9)
+            {
+                this.lblStatus.Text = "Just right!";
+                this.BackColor = Color.DarkBlue;
+                this.ForeColor = Color.White;
+            }
+            else if (waterQuantity >= 9)
+            {
+                this.lblStatus.Text = "Too much!";
+                this.BackColor = Color.Red;
+                this.ForeColor = Color.Yellow;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
